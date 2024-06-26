2b03e5f0ad0e3e514dec0fd181cf76182c0bf29f
AndersonTM30 <andersondev30@gmail.com>
2024-06-26 02:33:39 -0300

    feat: add route fill-template

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,9 +1,14 @@
 import { Router, Request, Response } from 'express';
+import { createFillTemplateController } from '../useCases/createFillTemplate';
 
 const router = Router();
 
 router.get('/', (req: Request, res: Response) => {
     res.send('Hello Dev!')
+})
+
+router.post('/fill-template', (request: Request, response: Response) => {
+    return createFillTemplateController.handle(request, response);
 })
 
 export { router }
