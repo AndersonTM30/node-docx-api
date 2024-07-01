8ada28980e7c0f5cf26c8e12a52e1ae44e25c982
AndersonTM30 <andersondev30@gmail.com>
2024-07-01 00:36:01 -0300

    refactor: add validation payload is not emppty

--- src/useCases/createFillTemplate/CreateFillTemplateController.ts
+++ src/useCases/createFillTemplate/CreateFillTemplateController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { CreateFillTemplateUseCase } from "./CreateFillTemplateUseCase";
+import { IFillTemplateDto } from "./IFillTemplateDto";
 
 export class CreateFillTemplateController {
     constructor(
@@ -7,8 +8,8 @@ export class CreateFillTemplateController {
     ){}
 
     async handle(request: Request, response: Response) {
-        const data = request.body;
-
+        const data: IFillTemplateDto = request.body;
+        if(Object.keys(data).length == 0) return response.status(400).json({ message: 'Data is not empty.'});
         try {
             let result = await this.createFillTemplateUseCase.execute(data)
             return response.status(200).download(`${result}`, 'filled_template.docx');
